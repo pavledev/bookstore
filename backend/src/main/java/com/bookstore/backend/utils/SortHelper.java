e8a9d97b5ddece25f5ceb59e71f785a609cbbff0
Pavle <pavle_nis@yahoo.com>
2025-05-21 23:10:19 +0200

    [BE] Add SortHelper

---
+++ backend/src/main/java/com/bookstore/backend/utils/SortHelper.java
@@ -0,0 +1,21 @@
+package com.bookstore.backend.utils;
+
+import org.springframework.data.domain.Sort;
+
+public class SortHelper
+{
+    public static Sort getSort(String sortKey)
+    {
+        if (sortKey == null) return Sort.unsorted();
+
+        return switch (sortKey)
+        {
+            case "title_asc" -> Sort.by(Sort.Direction.ASC, "title");
+            case "title_desc" -> Sort.by(Sort.Direction.DESC, "title");
+            case "price_asc" -> Sort.by(Sort.Direction.ASC, "price");
+            case "price_desc" -> Sort.by(Sort.Direction.DESC, "price");
+            case "created_desc" -> Sort.by(Sort.Direction.DESC, "createdAt");
+            default -> Sort.unsorted();
+        };
+    }
+}
