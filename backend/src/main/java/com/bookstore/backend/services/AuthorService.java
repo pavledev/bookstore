a22b282234cf6a2deefda30478527ecd7c6da64f
Pavle <pavle_nis@yahoo.com>
2025-06-02 08:13:47 +0200

    [BE] Implement create and update author functionality in AuthorService

--- backend/src/main/java/com/bookstore/backend/services/AuthorService.java
+++ backend/src/main/java/com/bookstore/backend/services/AuthorService.java
@@ -1,9 +1,12 @@
 package com.bookstore.backend.services;
 
+import com.bookstore.backend.dtos.request.CreateAuthorRequest;
+import com.bookstore.backend.dtos.request.UpdateAuthorRequest;
 import com.bookstore.backend.entities.Author;
 import com.bookstore.backend.mappers.AuthorMapper;
 import com.bookstore.backend.models.AuthorModel;
 import com.bookstore.backend.repositories.IAuthorRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -30,5 +33,26 @@ public class AuthorService implements IAuthorService
         List<Author> authors = authorRepository.findAllSortedByName();
 
         return authorMapper.toModelList(authors);
+    }
+
+    @Override
+    public void createAuthor(CreateAuthorRequest createAuthorRequest)
+    {
+        Author author = new Author();
+
+        author.setFullName(createAuthorRequest.getFullName());
+
+        authorRepository.save(author);
+    }
+
+    @Override
+    public void updateAuthor(Integer authorId, UpdateAuthorRequest updateAuthorRequest)
+    {
+        Author author = authorRepository.findById(authorId)
+                .orElseThrow(() -> new EntityNotFoundException("Author not found."));
+
+        author.setFullName(updateAuthorRequest.getFullName());
+
+        authorRepository.save(author);
     }
 }
