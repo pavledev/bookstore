c78474521394ffac268c07c77fe2bc07891cbd28
Pavle <pavle_nis@yahoo.com>
2025-06-02 08:25:30 +0200

    [BE] Add create and update methods to IAuthorService

--- backend/src/main/java/com/bookstore/backend/services/IAuthorService.java
+++ backend/src/main/java/com/bookstore/backend/services/IAuthorService.java
@@ -1,5 +1,7 @@
 package com.bookstore.backend.services;
 
+import com.bookstore.backend.dtos.request.CreateAuthorRequest;
+import com.bookstore.backend.dtos.request.UpdateAuthorRequest;
 import com.bookstore.backend.models.AuthorModel;
 
 import java.util.List;
@@ -7,5 +9,10 @@ import java.util.List;
 public interface IAuthorService
 {
     List<AuthorModel> getAuthors();
+
     List<AuthorModel> getAuthorsSortedByName();
+
+    void createAuthor(CreateAuthorRequest createAuthorRequest);
+
+    void updateAuthor(Integer authorId, UpdateAuthorRequest updateAuthorRequest);
 }
