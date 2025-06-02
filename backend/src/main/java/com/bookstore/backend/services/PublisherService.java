864d21a6a77713caa9b4b725ea91c61a7c8e4858
Pavle <pavle_nis@yahoo.com>
2025-06-02 08:31:04 +0200

    [BE] Implement create and update publisher functionality

--- backend/src/main/java/com/bookstore/backend/services/PublisherService.java
+++ backend/src/main/java/com/bookstore/backend/services/PublisherService.java
@@ -1,9 +1,12 @@
 package com.bookstore.backend.services;
 
+import com.bookstore.backend.dtos.request.CreatePublisherRequest;
+import com.bookstore.backend.dtos.request.UpdatePublisherRequest;
 import com.bookstore.backend.entities.Publisher;
 import com.bookstore.backend.mappers.PublisherMapper;
 import com.bookstore.backend.models.PublisherModel;
 import com.bookstore.backend.repositories.IPublisherRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +25,26 @@ public class PublisherService implements IPublisherService
         List<Publisher> publishers = publisherRepository.findAll();
 
         return publisherMapper.toModelList(publishers);
+    }
+
+    @Override
+    public void createPublisher(CreatePublisherRequest createPublisherRequest)
+    {
+        Publisher publisher = new Publisher();
+
+        publisher.setName(createPublisherRequest.getName());
+
+        publisherRepository.save(publisher);
+    }
+
+    @Override
+    public void updatePublisher(Integer publisherId, UpdatePublisherRequest updatePublisherRequest)
+    {
+        Publisher publisher = publisherRepository.findById(publisherId)
+                .orElseThrow(() -> new EntityNotFoundException("Publisher not found."));
+
+        publisher.setName(updatePublisherRequest.getName());
+
+        publisherRepository.save(publisher);
     }
 }
