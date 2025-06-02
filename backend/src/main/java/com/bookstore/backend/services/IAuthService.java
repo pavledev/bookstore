a40ddc6351738f3e591d3e43ec22dff323d3db84
Pavle <pavle_nis@yahoo.com>
2025-06-02 08:26:15 +0200

    [BE] Refactor IAuthService to return TokenResponse and void instead of ResponseEntity

--- backend/src/main/java/com/bookstore/backend/services/IAuthService.java
+++ backend/src/main/java/com/bookstore/backend/services/IAuthService.java
@@ -2,15 +2,16 @@ package com.bookstore.backend.services;
 
 import com.bookstore.backend.dtos.request.LoginRequest;
 import com.bookstore.backend.dtos.request.RegisterRequest;
+import com.bookstore.backend.dtos.response.TokenResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface IAuthService
 {
-    ResponseEntity<?> login(LoginRequest request);
+    TokenResponse login(LoginRequest request);
 
-    ResponseEntity<?> register(RegisterRequest request);
+    void register(RegisterRequest request);
 
-    ResponseEntity<?> refresh(String refreshToken);
+    TokenResponse refresh(String refreshToken);
 
-    ResponseEntity<?> logout(String refreshToken);
+    void logout(String refreshToken);
 }
