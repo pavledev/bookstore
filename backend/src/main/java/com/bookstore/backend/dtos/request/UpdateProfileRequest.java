f1626d44a0b40c478f1e3b86b535a22b365afbc1
Pavle <pavle_nis@yahoo.com>
2025-06-02 08:02:13 +0200

    [BE] Add UpdateProfileRequest

---
+++ backend/src/main/java/com/bookstore/backend/dtos/request/UpdateProfileRequest.java
@@ -0,0 +1,45 @@
+package com.bookstore.backend.dtos.request;
+
+import com.bookstore.backend.validators.PasswordMatches;
+import com.bookstore.backend.validators.UniqueEmail;
+import com.bookstore.backend.validators.UniqueUsername;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@UniqueEmail
+@UniqueUsername
+@Data
+public class UpdateProfileRequest
+{
+    @NotBlank(message = "Ime je obavezno")
+    @Size(min = 2, message = "Ime mora imati najmanje 2 slova")
+    private String firstName;
+
+    @NotBlank(message = "Prezime je obavezno")
+    @Size(min = 2, message = "Prezime mora imati najmanje 2 slova")
+    private String lastName;
+
+    @NotBlank(message = "Email je obavezan")
+    @Email(message = "Format email adrese nije validan")
+    private String email;
+
+    @NotBlank(message = "Korisničko ime je obavezno")
+    @Size(min = 6, message = "Korisničko ime mora imati najmanje 6 karaktera")
+    private String username;
+
+    @NotBlank(message = "Broj telefona je obavezan")
+    @Pattern(
+            regexp = "\\d{3}-\\d{2}-\\d{2}-\\d{3}",
+            message = "Broj telefona mora da bude u formatu 000-00-00-000"
+    )
+    private String phoneNumber;
+
+    @NotBlank(message = "Mesto je obavezno")
+    private String city;
+
+    @NotBlank(message = "Ulica je obavezna")
+    private String streetName;
+
+    @NotBlank(message = "Broj je obavezan")
+    private String streetNumber;
+}
