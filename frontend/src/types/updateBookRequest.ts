f540d185d9c3e0a2601935f3d3f6ff16a17a8ac1
Pavle <pavle_nis@yahoo.com>
2025-06-02 09:03:51 +0200

    [FE] Add UpdateBookRequest

---
+++ frontend/src/types/updateBookRequest.ts
@@ -0,0 +1,32 @@
+import { Category } from "@/types/category";
+import { Publisher } from "@/types/publisher";
+import { Author } from "@/types/author";
+import { Genre } from "@/types/genre";
+
+export interface UpdateBookRequest
+{
+    category?: Category | null;
+    publisher?: Publisher | null;
+    title: string;
+    mediumImagePath: string;
+    largeImagePath: string;
+    excerptPath?: string | null;
+    quantity: number;
+    description?: string | null;
+    publicationYear?: string | null;
+    importYear?: string | null;
+    binding?: string | null;
+    pageCount?: number | null;
+    script?: string | null;
+    weight?: string | null;
+    unit?: string | null;
+    isbn?: string | null;
+    barCode?: string | null;
+    importCountry?: string | null;
+    ageGroup?: string | null;
+    format?: string | null;
+    price: number;
+    discountPrice: number;
+    authors: Author[];
+    genres: Genre[];
+}
