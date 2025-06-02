17aab8d7f297826298558fa7baf54ec188896424
Pavle <pavle_nis@yahoo.com>
2025-06-02 08:28:58 +0200

    [BE] Add create and update methods to IPublisherService

--- backend/src/main/java/com/bookstore/backend/services/IPublisherService.java
+++ backend/src/main/java/com/bookstore/backend/services/IPublisherService.java
@@ -1,5 +1,7 @@
 package com.bookstore.backend.services;
 
+import com.bookstore.backend.dtos.request.CreatePublisherRequest;
+import com.bookstore.backend.dtos.request.UpdatePublisherRequest;
 import com.bookstore.backend.models.PublisherModel;
 
 import java.util.List;
@@ -7,4 +9,8 @@ import java.util.List;
 public interface IPublisherService
 {
     List<PublisherModel> getPublishers();
+
+    void createPublisher(CreatePublisherRequest createPublisherRequest);
+
+    void updatePublisher(Integer publisherId, UpdatePublisherRequest updatePublisherRequest);
 }
