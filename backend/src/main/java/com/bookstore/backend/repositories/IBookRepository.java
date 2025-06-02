9d852f3a52e94ae69d459e5bdf5d04fb2aed7f30
Pavle <pavle_nis@yahoo.com>
2025-06-02 08:10:24 +0200

    [BE] Add softDeleteById method in IBookRepository

--- backend/src/main/java/com/bookstore/backend/repositories/IBookRepository.java
+++ backend/src/main/java/com/bookstore/backend/repositories/IBookRepository.java
@@ -3,7 +3,13 @@ package com.bookstore.backend.repositories;
 import com.bookstore.backend.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IBookRepository extends JpaRepository<Book, Integer>, JpaSpecificationExecutor<Book>
 {
+    @Modifying
+    @Query("UPDATE Book b SET b.isDeleted = true WHERE b.bookId = :bookId")
+    void softDeleteById(@Param("bookId") Integer bookId);
 }
