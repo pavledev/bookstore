8cb07cac280c7f2f5871c6d28c389f9b8f32b213
Pavle <pavle_nis@yahoo.com>
2025-06-02 09:06:36 +0200

    [FE] Add AdminUpdateProfileRequest

---
+++ frontend/src/types/adminUpdateProfileRequest.ts
@@ -0,0 +1,12 @@
+export interface AdminUpdateProfileRequest
+{
+    firstName: string;
+    lastName: string;
+    email: string;
+    username: string;
+    phoneNumber: string;
+    city: string;
+    streetName: string;
+    streetNumber: string;
+    isAdmin: boolean;
+}
