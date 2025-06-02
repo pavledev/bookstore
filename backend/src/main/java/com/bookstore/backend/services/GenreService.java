08fe3c51ee6382c061e15cd40fa5f0ac2ff7ec4c
Pavle <pavle_nis@yahoo.com>
2025-06-02 08:23:41 +0200

    [BE] Implement create and update logic for genres

--- backend/src/main/java/com/bookstore/backend/services/GenreService.java
+++ backend/src/main/java/com/bookstore/backend/services/GenreService.java
@@ -1,9 +1,12 @@
 package com.bookstore.backend.services;
 
+import com.bookstore.backend.dtos.request.CreateGenreRequest;
+import com.bookstore.backend.dtos.request.UpdateGenreRequest;
 import com.bookstore.backend.entities.Genre;
 import com.bookstore.backend.mappers.GenreMapper;
 import com.bookstore.backend.models.GenreModel;
 import com.bookstore.backend.repositories.IGenreRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +25,28 @@ public class GenreService implements IGenreService
         List<Genre> genres = genreRepository.findAll();
 
         return genreMapper.toModelList(genres);
+    }
+
+    @Override
+    public void createGenre(CreateGenreRequest createGenreRequest)
+    {
+        Genre genre = new Genre();
+
+        genre.setName(createGenreRequest.getName());
+        genre.setDescription(createGenreRequest.getDescription());
+
+        genreRepository.save(genre);
+    }
+
+    @Override
+    public void updateGenre(Integer genreId, UpdateGenreRequest updateGenreRequest)
+    {
+        Genre genre = genreRepository.findById(genreId)
+                .orElseThrow(() -> new EntityNotFoundException("Genre not found."));
+
+        genre.setName(updateGenreRequest.getName());
+        genre.setDescription(updateGenreRequest.getDescription());
+
+        genreRepository.save(genre);
     }
 }
--- backend/src/main/java/com/bookstore/backend/services/IGenreService.java
+++ backend/src/main/java/com/bookstore/backend/services/IGenreService.java
@@ -1,5 +1,7 @@
 package com.bookstore.backend.services;
 
+import com.bookstore.backend.dtos.request.CreateGenreRequest;
+import com.bookstore.backend.dtos.request.UpdateGenreRequest;
 import com.bookstore.backend.models.GenreModel;
 
 import java.util.List;
@@ -7,4 +9,8 @@ import java.util.List;
 public interface IGenreService
 {
     List<GenreModel> getGenres();
+
+    void createGenre(CreateGenreRequest createGenreRequest);
+
+    void updateGenre(Integer genreId, UpdateGenreRequest updateGenreRequest);
 }
